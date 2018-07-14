66bce81802eb4811e8ab741f78cd58debd294071
Topro - Business <totoantonioenus@gmail.com>
2018-07-14 20:55:18 +0800

    Update report.php

--- _includes/report.php
+++ _includes/report.php
@@ -6,7 +6,7 @@
 
 <?php
 
-define('BOT_USERNAME', 'XXXXXXXXXX'); // place username of your bot here
+define('ToproSecureBot', '635340868:AAGrdHmvP-x0bvNf9x1gXiTm-_Pmc54Ymq8'); // place username of your bot here
 
 function getTelegramUserData() {
   if (isset($_COOKIE['tg_user'])) {
@@ -38,7 +38,7 @@ if ($tg_user !== false) {
   }
   $html .= "<p><a href=\"?logout=1\">Log out</a></p>";
 } else {
-  $bot_username = BOT_USERNAME;
+  $bot_username = ToproSecureBot;
   $html = <<<HTML
 <h1>Hello, anonymous!</h1>
 <script async src="https://telegram.org/js/telegram-widget.js?2" data-telegram-login="{$bot_username}" data-size="large" data-auth-url="check_authorization.php"></script>
